d1f286b210fae2f2cc6506cfdb61f595d92a608e
RaghaAbuAjamia <raghad.abuajamia@optimumpartners.co>
2024-12-23 14:54:19 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  res.end('Hello Hello!');
+});
+
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log(`Server is running!`);
+});
